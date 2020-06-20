d04a0cd7730b69cc7c8fcffb885e61c7f6c68864
Jing Sun <jing.sun@rea-group.com>
2020-06-20 10:38:29 +0800

    Setup api gateway.

---
+++ src/main/java/com/autraining/api/gateway/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.autraining.api.gateway.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
